27e06e101d37c52d1a31f758fc1c55ce9e8ba758
dejah <khadejahhylton@yahoo.com>
2018-02-22 03:40:00 +0000

    Lab complete :)

--- app/views.py
+++ app/views.py
@@ -65,6 +65,8 @@ def get_uploaded_images():
     
 @app.route('/files')
 def files():
+    if not session.get('logged_in'):
+        abort(401)
     f = get_uploaded_images()
     return render_template('files.html', files = f)
 
